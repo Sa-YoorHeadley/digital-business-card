2f75d9e385df41cd4b8909821561d50f4e54afac
unknown <sai.h152015@gmail.com>
2022-09-30 15:59:28 -0400

    Update css, Info and profile picture

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ProfilePicture from '../images/profile-picture.png'
+import ProfilePicture from '../images/profile-picture.jpg'
 
 export default function Info() {
-  return (<div className="info">
+  return (
+  <div className="info">
     <img className="profile-picture" src={ProfilePicture} alt ="Logo"/>
     <h1 className="name">Sa-Yoor Headley</h1>
     <h3 className="title">Frontend Developer</h3>
     <div class='btn-container'>
-    <button class="btn btn-email">Email</button>
-    <button class="btn btn-linkedin">LinkedIn</button>
+    <a href='mailto:sai.h152015@gmail.com' class="btn btn-email">Email</a>
+    <a href='https://www.linkedin.com/in/sa-yoor-headley/' class="btn btn-linkedin">LinkedIn</a>
     </div>
   </div>);
 }
